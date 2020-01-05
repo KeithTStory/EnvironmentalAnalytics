e08c7c27c62760fe97f0e3fd34d69c4bb49ebaf9
KeithTStor <keithtrizinstory@gmail.com>
2020-01-05 14:50:37 -0800

    Intiation

---
+++ ColumnAnalysis.R
@@ -0,0 +1,7 @@
+#The purpose of this script is to analyze the petroleum profiles of the bonny soil core samples compated to bonny crude.
+
+library(readr)
+library(tidyverse)
+
+
+raw <- read_csv("MasterDoc-graphing_NoSymbol.csv")
